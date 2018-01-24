1abd7e7f006d401ee69d03d42977f4ce7d6f4b79
Mihovska <vili_mihovska@live.com>
2018-01-24 14:30:53 +0200

    Creating initialization function

--- app.js
+++ app.js
@@ -9,20 +9,9 @@
 
  let scores, roundScore, activePlayer;
 
- scores = [0, 0];
- roundScore = 0;
- activePlayer = 0;
-
-
- document.querySelector('.dice').style.display = 'none';
-
- document.getElementById('score-0').textContent = '0';
- document.getElementById('score-1').textContent = '0';
- document.getElementById('current-0').textContent = '0';
- document.getElementById('current-1').textContent = '0';
+ init();
 
  document.querySelector('.btn-roll').addEventListener('click', function() {
-
     //1. Random number
     let dice = Math.floor(Math.random() * 6) + 1;
 
@@ -77,15 +66,30 @@
             document.querySelector('.player-1-panel').classList.toggle('active');
 
             document.querySelector('.dice').style.display = 'none';
-
-            //document.querySelector('.player-0-panel').classList.remove('active');
-            //document.querySelector('.player-1-panel').classList.add('active');
  }
 
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
 
+    document.querySelector('.dice').style.display = 'none';
 
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
  //document.querySelector('#current-' + activePlayer).textContent = dice;
  //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
